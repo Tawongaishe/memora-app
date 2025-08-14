5afe2c4b97e0f31c87dedf83da1f187f182106b3
Tawongaishe Nhawu <92609012+Tawongaishe@users.noreply.github.com>
2025-08-13 17:09:50 -0700

    remove trailing slash

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -37,7 +37,7 @@ def create_app(config_name=None):
     migrate.init_app(app, db)
     
     cors.init_app(app, 
-              origins=['http://localhost:5173', 'https://memora-app-wawu.vercel.app/'],  # Add both localhost variants
+              origins=['http://localhost:5173', 'https://memora-app-wawu.vercel.app'],  # Add both localhost variants
               allow_headers=['Content-Type', 'Authorization', 'X-Guest-Session'],
               methods=['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'])
     
